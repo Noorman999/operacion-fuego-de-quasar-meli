ebbbfc05033bbc2541a539651e73c77f8a22d74d
Noorman999 <normanfunes@outlook.com>
2022-04-19 18:20:21 -0300

    Index Added

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from resources import api
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 api.init_app(app)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
